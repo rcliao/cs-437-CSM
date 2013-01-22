4b32414447b2d55253c53bde661c3fb315a3badc
Eric Liao <rcliao01@gmail.com>
2013-01-21 18:38:02 -0800

    set up the routing and add announcement page

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,11 @@
 'use strict';
 
 // practice to add module for route control as store
-angular.module('csm', [])
+angular.module('csm', []).
+	config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/login', {templateUrl: 'partials/login.html',   controller: LoginCtrl}).
+		when('/main', {templateUrl: 'partials/menu.html',   controller: MenuCtrl}).
+		when('/announcement', {templateUrl: 'partials/announcement.html',   controller: AnnoCtrl}).
+		otherwise({redirectTo: '/main'});
+}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,3 +4,20 @@
 function LoginCtrl($scope) {
 	
 }
+
+function MainCtrl($scope) {
+}
+
+function MenuCtrl($scope) {
+
+}
+
+function AnnoCtrl($scope) {
+	$scope.announcements = [
+		{head:"news", context:"something here"},
+		{head:"news", context:"something here"},
+		{head:"news", context:"something here"},
+		{head:"news", context:"something here"},
+		{head:"news", context:"something here"},
+	];
+}
